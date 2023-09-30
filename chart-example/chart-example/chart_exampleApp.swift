8de290b011c3e313858e676847794f0e2118e0d2
dtrognn <dtrognn@gmail.com>
2023-09-30 10:41:29 +0700

    init project

---
+++ chart-example/chart-example/chart_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  chart_exampleApp.swift
+//  chart-example
+//
+//  Created by dtrognn on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct chart_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
